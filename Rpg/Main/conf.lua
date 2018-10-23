acf2ba2d4827fafd1e86069d3e6e7799f3df5c0f
Dustin Neumann <neumdu@csl07.cs.wallawalla.edu>
2018-10-23 13:22:49 -0700

    RPG

---
+++ Rpg/Main/conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.width = 1280
+    t.window.height= 720
+    t.window.title = "DevWindow"
+    t.console = true
+end
---
+++ Rpg/Main/main.lua
@@ -0,0 +1,3 @@
+function love.draw()
+    love.graphics.rectangle("fill", 0, 0, 100, 100)
+end
